3b9e855da4387bd06c8fcfcd36019f0a2dfa26f6
Nikitha303 <tcsnew1223@gmail.com>
2022-01-20 15:57:51 +0530

    validation changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.IUserService;
+
+
 
 @RestController
 public class UserController {
@@ -16,13 +25,19 @@ public class UserController {
 	IUserService userService;
     //new
 	@GetMapping("/user")
-	String getUser() {
-		System.out.println("testing");
-		return "testing";
+	Iterable<User> getUsers() {
+		return userService.getUsers();
+	
 	}
+	@GetMapping("/user/{id}")
+    Optional<User> getUsers(@PathVariable("id") Integer id){
+			return userService.getUser(id);
+	}
+	
 
 	@PostMapping("/user") // create
-	void createUser(@RequestBody User user) {
+	@ResponseStatus(code=HttpStatus.CREATED)
+	void createUser(@RequestBody @Valid User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
 		userService.saveUser(user);
--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -1,10 +1,16 @@
 package com.example.demo.service;
 
+
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.UserRepository;
 import com.example.demo.entity.User;
+
+
 @Service
 public class IUserService {
 	@Autowired //DI
@@ -12,5 +18,10 @@ public class IUserService {
 	public void saveUser(User user) {
 		userRepository.save(user);
 	}
-
+     public Iterable<User> getUsers(){
+    	 return userRepository.findAll();
+     }
+     public Optional<User> getUser(Integer id){
+    	 return userRepository.findById(id);
+     }
 }
